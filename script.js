8a665ed4eda9d91039e6f9bc2caa6dbf0bcfe25f
unknown <kauauwc@gmail.com>
2024-01-31 19:46:24 -0300

    Atualizacao 1

---
+++ script.js
@@ -0,0 +1,29 @@
+function abrirIntroducao() {
+    const modal = document.getElementById('modalIntroducao');
+    modal.style.display = 'block';
+}
+
+function fecharIntroducao() {
+    const modal = document.getElementById('modalIntroducao');
+    modal.style.display = 'none';
+}
+
+function abrirCandidatando() {
+    const modal = document.getElementById('modalCandidatando');
+    modal.style.display = 'block';
+}
+
+function fecharCandidatando() {
+    const modal = document.getElementById('modalCandidatando');
+    modal.style.display = 'none';
+}
+
+function abrirExpectativa() {
+    const modal = document.getElementById('modalExpectativa');
+    modal.style.display = 'block';
+}
+
+function fecharExpectativa() {
+    const modal = document.getElementById('modalExpectativa');
+    modal.style.display = 'none';
+}
