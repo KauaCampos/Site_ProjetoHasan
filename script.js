0f89a6ec602fe521e61704309e1949c3de0bb37a
unknown <kauauwc@gmail.com>
2024-02-01 21:23:30 -0300

    Atualizacao Mudar Cores

--- script.js
+++ script.js
@@ -27,3 +27,53 @@ function fecharExpectativa() {
     const modal = document.getElementById('modalExpectativa');
     modal.style.display = 'none';
 }
+
+let menuAberto = false;
+
+function abrirMenu() {
+    const menuOpcoes = document.getElementById('menuOpcoes');
+    menuOpcoes.style.display = menuAberto ? 'none' : 'block';
+    menuAberto = !menuAberto;
+}
+
+let coresNeonAtivadas = false;
+
+function alternarCoresNeon() {
+    const bolhasPares = document.querySelectorAll('.bolhas span:nth-of-type(even)');
+    const bolhasImpares = document.querySelectorAll('.bolhas span:nth-of-type(odd)');
+
+    if (!coresNeonAtivadas) {
+        bolhasPares.forEach((span, i) => {
+            const corNeon = getCorNeon(i);
+            span.style.background = corNeon;
+            span.style.boxShadow = `0 0 0 10px ${corNeon}44, 0 0 50px ${corNeon}, 0 0 100px ${corNeon}`;
+        });
+
+        bolhasImpares.forEach((span, i) => {
+            const corNeon = getCorNeon(i + bolhasPares.length);
+            span.style.background = corNeon;
+            span.style.boxShadow = `0 0 0 10px ${corNeon}44, 0 0 50px ${corNeon}, 0 0 100px ${corNeon}`;
+        });
+
+        coresNeonAtivadas = true;
+    } 
+    
+    else {
+        bolhasPares.forEach(span => {
+            span.style.background = '#2dc3ff'; // Cor azul original
+            span.style.boxShadow = '0 0 0 10px #2dc3ff44, 0 0 50px #2dc3ff, 0 0 100px #2dc3ff'; // Sombra azul original
+        });
+
+        bolhasImpares.forEach(span => {
+            span.style.background = '#e5ff00'; // Cor amarela original
+            span.style.boxShadow = '0 0 0 10px #e5ff0044, 0 0 50px #e5ff00, 0 0 100px #e5ff00'; // Sombra amarela original
+        });
+
+        coresNeonAtivadas = false;
+    }
+}
+
+function getCorNeon(i) {
+    const coresNeon = ['#ff66ff', '#00ffff', '#ffff66', '#66ff66']; // Cores neon
+    return coresNeon[i % coresNeon.length];
+}
